35396ca54da2485e7024029cc94bf2d3ded6a925
SunLee128 <sunlee128@gmail.com>
2020-08-09 10:28:49 +1000

    added structural patterns

--- creationalPatterns/abstractFactory.js
+++ creationalPatterns/abstractFactory.js
@@ -1,4 +1,3 @@
-
 class Car {
   constructor (doors, engine, color) {
     this.doors = doors;
@@ -29,10 +28,87 @@ class SUV {
 class SUVFactory {
   createCar (type) {
     switch (type) {
-      case 'civic':
+      case 'cx5':
         return new Car(4, 'V6', 'red');
-      case 'honda':
+      case 'santa fe':
         return new Car(2, 'V8', 'red');
     }
   }
 }
+
+const carFactory = new CarFactory();
+const suvFactory = new SUVFactory();
+
+const autoManufacturer = (type, model) => {
+  switch (type) {
+    case 'car':
+      return carFactory.createCar(model);
+    case 'suv':
+      return suvFactory.createCar(model);
+  }
+};
+
+const cx5 = autoManufacturer('suv', 'cx5');
+console.log(cx5);
+
+function Employee (name) {
+  this.name = name;
+
+  this.say = function () {
+    log.add('I am employee ' + name);
+  };
+}
+
+function EmployeeFactory () {
+  this.create = function (name) {
+    return new Employee(name);
+  };
+}
+
+function Vendor (name) {
+  this.name = name;
+
+  this.say = function () {
+    log.add('I am vendor ' + name);
+  };
+}
+
+function VendorFactory () {
+  this.create = function (name) {
+    return new Vendor(name);
+  };
+}
+
+// log helper
+var log = (function () {
+  var log = '';
+
+  return {
+    add  : function (msg) {
+      log += msg + '\n';
+    },
+    show : function () {
+      console.log(log);
+      log = '';
+    },
+  };
+})();
+
+function run () {
+  var persons = [];
+  var employeeFactory = new EmployeeFactory();
+  var vendorFactory = new VendorFactory();
+
+  persons.push(employeeFactory.create('Joan DiSilva'));
+  persons.push(employeeFactory.create("Tim O'Neill"));
+  persons.push(vendorFactory.create('Gerald Watson'));
+  persons.push(vendorFactory.create('Nicole McNight'));
+
+  for (var i = 0, len = persons.length; i < len; i++) {
+    persons[i].say();
+  }
+
+  log.show();
+}
+
+run();
